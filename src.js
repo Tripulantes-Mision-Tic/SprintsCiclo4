91fe7dfb97d747dfc1c6a7473009a1a0fe636855
Marcos Correa <marcoscinturon@gmail.com>
2021-11-17 16:48:26 -0500

    plantilla para el sprint 2

---
+++ src.js
@@ -0,0 +1,24 @@
+
+
+const calcularPorcentajes = (limitePM10, limitePM2punto5, limiteNO2, limiteCO, limiteS02, valorPM10, valorPM2punto5, valorNO2, valorCO, valorS02) => {
+    
+}
+
+
+
+const registrarAQI = (value) => {
+
+}
+
+
+//Función extra:
+const registrarLimite = (limitePM10, limitePM2punto5, limiteNO2, limiteCO, limiteS02, valorPM10, valorPM2punto5, valorNO2, valorCO, valorS02) => {
+
+}
+
+
+
+
+
+module.exports.registrarAQI= registrarAQI;
+module.exports.calcularPorcentajes= calcularPorcentajes;
