54bc74193f98847d431c98a6d850438c62a31b0b
Marcos Correa <marcoscinturon@gmail.com>
2021-11-18 18:00:54 -0500

    correccion de logica funcion extra

--- src.js
+++ src.js
@@ -20,20 +20,22 @@ const registrarAQI = (value) => {
 
 
 //Función extra:
-/*const registrarLimite = (limitePM10, limitePM2punto5, limiteNO2, limiteCO, limiteS02, valorPM10, valorPM2punto5, valorNO2, valorCO, valorS02) => {
+/*function registrarLimite (limitePM10, limitePM2punto5, limiteNO2, limiteCO, limiteS02, valorPM10, valorPM2punto5, valorNO2, valorCO, valorS02) {
     
-    if (valorPM10>limitePM10 && limitePM10){
-        return ("¡Se superaron los límites de PM10 !")
-    }else if (valorPM2punto5>limitePM2punto5 && limitePM2punto5){
-        return ("¡Se superaron los límites de PM2.5 !")
-    }else if (valorNO2>limiteNO2 && limiteNO2){
-        return ("¡Se superaron los límites de NO2 !")
-    }else if (valorCO>limiteCO && limiteCO){
-        return ("¡Se superaron los límites de CO !")
-    }else if (valorS02>limiteS02 && limiteS02){
-        return ("¡Se superaron los límites de SO2 !")
+    console.log( encontrarLimites(valorPM10,limitePM10, "PM10"))
+    console.log(encontrarLimites(valorPM2punto5, limitePM2punto5, "PM2.5"))
+    console.log(encontrarLimites(valorNO2, limiteNO2, "NO2"))
+    console.log(encontrarLimites(valorCO, limiteCO, "CO"))
+    console.log(encontrarLimites(valorS02,limiteS02, "S02"))
+    
+}
+
+
+function encontrarLimites (valor, limite, particula){
+    if (limite && valor>limite){
+        return ("¡Se superaron los límites de " + particula + " !")
     }else{
-        return ("Ninguna partícula sobrepasa los límites")
+        return (":)")
     }
 }*/
 
