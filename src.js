f88e1d81ea1f420df013e1b20e1beda13d053382
Tripulantes-Mision-Tic <90871557+Tripulantes-Mision-Tic@users.noreply.github.com>
2021-11-19 22:49:32 -0500

    Laotra (#1)
    * codigo completo sprint2
    
    * entregable sprint 2
    
    Co-authored-by: Marcos Correa <marcoscinturon@gmail.com>

--- src.js
+++ src.js
@@ -8,14 +8,43 @@
 ]*/
 
 
-const calcularPorcentajes = (limitePM10, limitePM2punto5, limiteNO2, limiteCO, limiteS02, valorPM10, valorPM2punto5, valorNO2, valorCO, valorS02) => {
+const calcularPocentajes = (limitePM10, limitePM2punto5, limiteNO2, limiteCO, limiteS02, valorPM10, valorPM2punto5, valorNO2, valorCO, valorS02) => {
     
+    let porcentajePM10 = porcentaje(limitePM10, valorPM10);
+    let porcentajePM2punto5 = porcentaje(limitePM2punto5, valorPM2punto5);
+    let porcentajeNO2 = porcentaje(limiteNO2,valorNO2);
+    let porcentajeCO = porcentaje(limiteCO, valorCO);
+    let porcentajeS02 = porcentaje(limiteS02, valorS02);
+  
+    return {
+        porcentajePM10,porcentajePM2punto5,porcentajeNO2,porcentajeCO,porcentajeS02
+    }
+}
+
+const porcentaje = (max, actual) => {
+    if (max && max != 0){
+        return actual*100/max;
+    } else {
+        return 0
+    } 
 }
 
 
 
 const registrarAQI = (value) => {
 
+    if (value>0 && value<300){
+        rangosAQI.forEach(element => {
+        
+            if (value >= element.de && value <= element.hasta){
+                cualitativo = element.etiqueta
+            }
+            
+        })
+        return cualitativo
+    }else{
+        return "fuera_de_rango"
+    }
 }
 
 
@@ -41,11 +70,15 @@ function encontrarLimites (valor, limite, particula){
 
 
 
-
-
-
 module.exports.registrarAQI= registrarAQI;
-module.exports.calcularPorcentajes= calcularPorcentajes;
+module.exports.calcularPocentajes= calcularPocentajes;
+
+
+
+
+
+
+
 
 
 
